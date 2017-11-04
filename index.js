c247baa00e094ca32e70f6dafc1cffd30754f499
Pranav Bokey <pranavbokey@Pranavs-MacBook-Pro.local>
2017-11-04 14:51:40 -0400

    refactored code

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
-import * as firebase from 'firebase';
-const firebaseConfig = {
-    apiKey: "AIzaSyCCzhkQsZBQfep8NSb9xfjyHRyaEkYJ0nI",
-    authDomain: "stacks-94005.firebaseapp.com",
-    databaseURL: "https://stacks-94005.firebaseio.com",
-    projectId: "stacks-94005",
-    storageBucket: "",
-    messagingSenderId: "400552345256"
-  };
-const firebaseApp = firebase.initializeApp(firebaseConfig);
+import App from './src/App';
 
 AppRegistry.registerComponent('Stacks', () => App);
