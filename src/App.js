b3077575ec943f6d331f49415d566ab1e8f9b6ad
Pranav Bokey <pranavbokey@Pranavs-MacBook-Pro.local>
2017-11-04 14:55:24 -0400

    folder is here now

---
+++ src/App.js
@@ -0,0 +1,68 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import * as firebase from 'firebase';
+const firebaseConfig = {
+    apiKey: "AIzaSyCCzhkQsZBQfep8NSb9xfjyHRyaEkYJ0nI",
+    authDomain: "stacks-94005.firebaseapp.com",
+    databaseURL: "https://stacks-94005.firebaseio.com",
+    projectId: "stacks-94005",
+    storageBucket: "",
+    messagingSenderId: "400552345256"
+  };
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit App.js
+        </Text>
+        <Text style={styles.instructions}>
+          {instructions}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
