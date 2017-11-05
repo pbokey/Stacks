2380c65dcc66aa363db2700916df597365e6a31a
Pranav Bokey <pranavbokey@Pranavs-MacBook-Pro.local>
2017-11-04 23:09:25 -0400

    moved uid to inside rer function

--- src/MainPage.js
+++ src/MainPage.js
@@ -8,18 +8,16 @@ import { Container, Header, Content, Form, Item, Input, Label, Button, Left, Rig
 
 
 export default class MainScreen extends React.Component<{}> {
-  componentDidMount() {
-    const { state } = this.props.navigation;
-    var uid = state.params.uid;
-  }
   static navigationOptions = {
     title: 'Main Page',
   };
   render() {
+    const { state } = this.props.navigation;
+    var uid = state.params.uid;
     return (
       <Container>
         <Content>
-          <Text>uid</Text>
+          <Text>{uid}</Text>
         </Content>
       </Container>
     );
